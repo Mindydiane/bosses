3bebef4f421521fb9a4be9cd18beeb74d1a90e14
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-17 14:15:11 -0500

    valid message for first 2 questions

--- index.js
+++ index.js
@@ -14,12 +14,39 @@ const questions = [
 {
   type: "input",
   name: "id",
-  message: "Enter Employee's ID: (*)"
+  message: "Enter Employee's ID: (*)",
+  validate: validId => {
+    if (validId) {
+      let id = parseInt(validID);
+      if (Number.isInteger(id)) {
+        return true;
+      }
+      else {
+        console.log("ID must be 0-9 digits only");
+        return false;
+      }
+    }
+  }
 },
 {
   type: "input",
   name: "name",
-  message: "Enter Employee's name: (*)"
+  message: "Enter Employee's name: (*)",
+  validate: validName => {
+    if (validName) {
+      let letters = /^[A-Z a-z]+$/;
+      if (validName.match(letters)) {
+        return true;
+      }
+      else {
+        console.log("Name should have alphabet letters only")
+        return false;
+      }
+    }
+    else {
+      console.log("Employee's name is required");
+    }
+  }
 },
 {
   type: "input",
