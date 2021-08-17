a44edf225e38e5124efccdfab5216408ba59277e
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-17 14:46:23 -0500

    managerQuestions added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//npm packages 
+//npm packages
 const inquirer = require("inquirer");
 const fs = require("fs");
 
@@ -11,78 +11,92 @@ const Engineer = require("./lib/Engineer");
 const employees = [];
 
 const questions = [
-{
-  type: "input",
-  name: "id",
-  message: "Enter Employee's ID: (*)",
-  validate: validId => {
-    if (validId) {
-      let id = parseInt(validID);
-      if (Number.isInteger(id)) {
-        return true;
-      }
-      else {
-        console.log("Must be 0-9 digits only");
-        return false;
-      }
-    }
-  }
-},
-{
-  type: "input",
-  name: "name",
-  message: "Enter Employee's name: (*)",
-  validate: validName => {
-    if (validName) {
-      let letters = /^[A-Z a-z]+$/;
-      if (validName.match(letters)) {
-        return true;
-      }
-      else {
-        console.log("Name should have alphabet letters only")
-        return false;
-      }
-    }
-    else {
-      console.log("Employee's name is required");
-    }
-  }
-},
-{
-  type: "input",
-  name: "email",
-  message: "Enter Employee's email address: (*)",
-  validate: validEmail=>
   {
-    if(validEmail)
-    {
-      //string@anystring.any string S
-        let email= /\S+@\S+\.\S+/;
-        if(validEmail.match(email))
-        {
-            return true;
+    type: "input",
+    name: "id",
+    message: "Enter Employee's ID: (*)",
+    validate: (validId) => {
+      if (validId) {
+        let id = parseInt(validID);
+        if (Number.isInteger(id)) {
+          return true;
+        } else {
+          console.log("Must be 0-9 digits only");
+          return false;
         }
-        else {
-            console.log("Invalid Emaill Address");
-        }
-    }
-      else {
-    console.log("Email is required!");
-    return false;
       }
-    }
+    },
   },
-{
-  type: "checkbox",
-  name: "role",
-  message: "Select the Employee's role: (*)",
-  choices: ['Engineer', 'Manager', 'Intern']
-  }
-]
+  {
+    type: "input",
+    name: "name",
+    message: "Enter Employee's name: (*)",
+    validate: (validName) => {
+      if (validName) {
+        let letters = /^[A-Z a-z]+$/;
+        if (validName.match(letters)) {
+          return true;
+        } else {
+          console.log("Name should have alphabet letters only");
+          return false;
+        }
+      } else {
+        console.log("Employee's name is required");
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter Employee's email address: (*)",
+    validate: (validEmail) => {
+      if (validEmail) {
+        //string@anystring.any string S
+        let email = /\S+@\S+\.\S+/;
+        if (validEmail.match(email)) {
+          return true;
+        } else {
+          console.log("Invalid Emaill Address");
+        }
+      } else {
+        console.log("Email is required!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "checkbox",
+    name: "role",
+    message: "Select the Employee's role: (*)",
+    choices: ["Engineer", "Manager", "Intern"],
+  },
+];
 
-function startPoint()
-const startPoint =()=>
-{
+const managerQuestions = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter manager's office number",
+      validate: (validOffiNumber) => {
+        if (validOffiNumber) {
+          let num = parseInt(validOffiNumber);
+          if (Number.isInteger(num)) {
+            return true;
+          } else {
+            console.log("Must be 0-9 digits only");
+            return false;
+          }
+        } else {
+          console.log("Office Number is required!");
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+const startPoint = () => {
   inquirer.prompt(questions);
-}
+};
 startPoint();
