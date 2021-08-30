f7553559100f3345bb2377930b369fc23d4195de
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-29 20:58:27 -0500

    started page template

--- index.js
+++ index.js
@@ -1,101 +1,54 @@
+//generate html 
+const {writeFile, copyFile } = require('./')
+
 //npm packages
 const inquirer = require("inquirer");
-const fs = require("fs");
 
 //importing classes
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
-const { type } = require("os");
+
 
 // all employees data storage
 const emp = [];
 
-// const questions = [
-//   {
-//     type: "input",
-//     name: "id",
-//     message: "Enter Employee's ID: (*)",
-//     validate: (validId) => {
-//       if (validId) {
-//         let id = parseInt(validID);
-//         if (Number.isInteger(id)) {
-//           return true;
-//         } else {
-//           console.log("Must be 0-9 digits only");
-//           return false;
-//         }
-//       }
-//     },
-//   },
-//   {
-//     type: "input",
-//     name: "name",
-//     message: "Enter Employee's name: (*)",
-//     validate: (validName) => {
-//       if (validName) {
-//         let letters = /^[A-Z a-z]+$/;
-//         if (validName.match(letters)) {
-//           return true;
-//         } else {
-//           console.log("Name should have alphabet letters only");
-//           return false;
-//         }
-//       } else {
-//         console.log("Employee's name is required");
-//       }
-//     },
-//   },
-//   {
-//     type: "input",
-//     name: "email",
-//     message: "Enter Employee's email address: (*)",
-//     validate: (validEmail) => {
-//       if (validEmail) {
-//         //string@anystring.any string S
-//         let email = /\S+@\S+\.\S+/;
-//         if (validEmail.match(email)) {
-//           return true;
-//         } else {
-//           console.log("Invalid Emaill Address");
-//         }
-//       } else {
-//         console.log("Email is required!");
-//         return false;
-//       }
-//     },
-//   },
-//   {
-//     type: "checkbox",
-//     name: "role",
-//     message: "Select the Employee's role: (*)",
-//     choices: ["Engineer", "Manager", "Intern"],
-//   },
-// ];
+const questions = [
+  {
+    type: "input",
+    name: "id",
+    message: "Enter Employee's ID: (*)",
+    
+    type: "checkbox",
+    name: "role",
+    message: "Select the Employee's role: (*)",
+    choices: ["Engineer", "Manager", "Intern"],
+  },
+];
 
-// const mgrQuestions = () => {
-//   inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "officeNumber",
-//       message: "Enter manager's office number",
-//       validate: (validOffiNumber) => {
-//         if (validOffiNumber) {
-//           let num = parseInt(validOffiNumber);
-//           if (Number.isInteger(num)) {
-//             return true;
-//           } else {
-//             console.log("Must be 0-9 digits only");
-//             return false;
-//           }
-//         } else {
-//           console.log("Office Number is required!");
-//           return false;
-//         }
-//       },
-//     },
-//   ]);
-// };
+const mgrQuestions = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter manager's office number",
+      validate: (validOffiNumber) => {
+        if (validOffiNumber) {
+          let num = parseInt(validOffiNumber);
+          if (Number.isInteger(num)) {
+            return true;
+          } else {
+            console.log("Must be 0-9 digits only");
+            return false;
+          }
+        } else {
+          console.log("Office Number is required!");
+          return false;
+        }
+      },
+    },
+  ]);
+};
 
 // const engQuestions = () => {
 //   inquirer.prompt([
@@ -151,7 +104,7 @@ const emp = [];
 //   });
 // };
 
-// const startPoint = () => {
+// const run = () => {
 //   addEmp();
 //   inquirer
 //     .prompt({
@@ -168,4 +121,4 @@ const emp = [];
 //     });
 // };
 
-// startPoint();
+// run();
---
+++ src/page-template.js
@@ -0,0 +1,9 @@
+
+const generateEmp = (info) => {
+    if (!info) {
+        return "";
+    }
+
+    return ``
+
+}
