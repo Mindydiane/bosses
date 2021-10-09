281781f4e949da298d15651a5b1aef1736c60f6c
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-08 20:44:51 -0500

    build page function

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
 
 //create promises with a function
-const writeFile = (fileContent) => {
+function writeFile(filename, employeeData) {
   return new Promise((resolve, reject) => {
-    fs.writeFile("./dist/index.html", fileContent, (err) => {
+    fs.writeFile('./dist/index.html', err => {
       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
       if (err) {
         reject(err);
         // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
         return;
       }
+
       // if everything went well, resolve the Promise and send the successful data to the `.then()` method
       resolve({
         ok: true,
-        message: "File created!",
+        message: 'File created!'
       });
     });
   });
@@ -21,17 +22,15 @@ const writeFile = (fileContent) => {
 // copying file
 const copyFile = () => {
   return new Promise((resolve, reject) => {
-    fs.copyFile('./src/style.css', './dist/style.css', (err) => {
-      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
       if (err) {
         reject(err);
-        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
         return;
       }
-      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+
       resolve({
         ok: true,
-        message: "Stylesheet created!",
+        message: 'Stylesheet created!'
       });
     });
   });
