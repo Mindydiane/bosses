88d86959b3f48524908e7c0db33d30c84f08899a
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-20 11:58:34 -0500

    update template generate engineer setup

--- src/page-template.js
+++ src/page-template.js
@@ -3,6 +3,25 @@ const teamBuild = (team) => {
   const teamInfo = [];
 
   const generateManager = (manager) => {
-    return ``
+    return `
+    <div class="card bg-primary shadow p-1 mx-2 my-2" style="width: 18rem;">
+      <div class="card-header text-white">
+        <h3>${manager.name} </h3>
+        <h4> <i class="fas fa-coffee"></i>  Manager</h4>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+      </ul>
+    </div>
+    `;
+  };
+
+  const generateEngineer = (engineer) => {
+    return `
+    
+    `
   }
-}
+
+};
